b4a24734b97a6d8ac637768e4a26e04d6f3eecf3
kr0st <kr0st@bioreactor.me>
2018-05-24 19:23:32 +0300

    Task findAllActiveUsersGroupedByFirstNicknameLetter done.

--- src/main/java/lv/javaguru/courses/ingenico/lecture4/streams/hometask/FilterAndCollectUsers.java
+++ src/main/java/lv/javaguru/courses/ingenico/lecture4/streams/hometask/FilterAndCollectUsers.java
@@ -3,6 +3,7 @@ package lv.javaguru.courses.ingenico.lecture4.streams.hometask;
 import lv.javaguru.courses.ingenico.lecture4.common.User;
 import lv.javaguru.courses.ingenico.lecture4.common.UserRepository;
 
+import javax.swing.plaf.synth.SynthEditorPaneUI;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
@@ -10,6 +11,17 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class FilterAndCollectUsers {
+
+
+    public static class UserComparator implements Comparator<User>
+    {
+
+        @Override
+        public int compare(User u1, User u2)
+        {
+            return -1 * u1.getNickname().compareTo(u2.getNickname());
+        }
+    }
 
     /*
     * TODO :
@@ -21,9 +33,21 @@ public class FilterAndCollectUsers {
     * use toMap or groupingBy -> https://www.mkyong.com/java8/java-8-collectors-groupingby-and-mapping-example/
     *
     * */
-    public Map<Character, List<User>> findAllActiveUsersGroupedByFirstNicknameLetter(UserRepository repository) {
-        return new HashMap<>();
+    public Map<Character, List<User>> findAllActiveUsersGroupedByFirstNicknameLetter(UserRepository repository)
+    {
+        UserComparator cmp = new UserComparator();
+        List<User> users = repository.getAll();
+        return users.stream()
+                .sorted(cmp::compare)
+                .collect(Collectors.groupingBy(user -> user.getNickname().toCharArray()[0]));
     }
 
+    public static void main(String[] args)
+    {
+        UserRepository userRepository = UserRepository.getInstance();
+        FilterAndCollectUsers userfilter  = new FilterAndCollectUsers();
 
+        HashMap<Character, List<User>> res = (HashMap<Character, List<User>>) userfilter.findAllActiveUsersGroupedByFirstNicknameLetter(userRepository);
+        System.out.println(res.toString());
+    }
 }
