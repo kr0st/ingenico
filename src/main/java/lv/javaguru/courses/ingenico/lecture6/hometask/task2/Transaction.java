bd95ef31e1bd11a11988b88b5f85a391fc5a7845
olegssedacs <olegs.sedacs@meawallet.com>
2018-05-30 11:04:40 +0300

    lecture 6 : hometask 2, fixed json field names

--- src/main/java/lv/javaguru/courses/ingenico/lecture6/hometask/task2/Transaction.java
+++ src/main/java/lv/javaguru/courses/ingenico/lecture6/hometask/task2/Transaction.java
@@ -8,7 +8,7 @@ import java.math.BigDecimal;
 public class Transaction {
 
     private String id;
-    private String cardId;
+    private String card;
     private String beneficiaryAccount;
     private BigDecimal amount;
 
