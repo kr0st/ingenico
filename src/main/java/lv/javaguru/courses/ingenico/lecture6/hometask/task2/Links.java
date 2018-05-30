d1ea4dfbd4963e8b809f0fedaceb6f6824b719d4
olegssedacs <olegs.sedacs@meawallet.com>
2018-05-30 10:18:08 +0300

    lecture 6 : hometask 2, links class

---
+++ src/main/java/lv/javaguru/courses/ingenico/lecture6/hometask/task2/Links.java
@@ -0,0 +1,11 @@
+package lv.javaguru.courses.ingenico.lecture6.hometask.task2;
+
+import lombok.Data;
+
+@Data
+public class Links {
+
+    private String cards;
+    private String transactions;
+
+}
